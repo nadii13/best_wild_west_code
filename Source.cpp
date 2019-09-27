12bae7a995014ed334a0b72c7cedf407de6c48bc
Nadya Loban <nadiezhda.loban@gmail.com>
2019-09-27 12:14:01 +0300

    Revert "finish"
    This reverts commit e248e25ae024e10d7c5fc9cf0c236b6786a15392.

--- Source.cpp
+++ Source.cpp
@@ -4,7 +4,5 @@ using namespace std;
 int main() {
 	cout << "Hello!" << endl;
 	cout << "It's me" << endl;
-	int b = 12;
-	------------------------------------------------
-	-------------------------------------
+	int b;
 }
